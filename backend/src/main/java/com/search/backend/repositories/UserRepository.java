d2a3c4b81f616d6d1a9e92c6b961036500b3dc31
Vadossss <iv.gav789@mail.ru>
2025-03-22 00:54:03 +0300

    Создан интерфейс для связи с таблицей пользователей

---
+++ backend/src/main/java/com/search/backend/repositories/UserRepository.java
@@ -0,0 +1,20 @@
+package com.search.backend.repositories;
+
+import com.search.backend.models.AppUser;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<AppUser, Integer> {
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    AppUser findByUsername(String username);
+
+    @Transactional
+    void deleteByUsername(String username);
+}
+
