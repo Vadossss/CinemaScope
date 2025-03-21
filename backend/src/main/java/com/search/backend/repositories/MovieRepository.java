c65e325077a468f5278cb40002e7ef15e7774687
Vadossss <iv.gav789@mail.ru>
2025-03-22 00:59:25 +0300

    Удален старый интерфейс таблицы пользователей

--- backend/src/main/java/com/search/backend/repositories/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.search.backend.repositories;
-
-import com.search.backend.models.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
