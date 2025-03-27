feed7a0f102bf3561baf2438c5fcdaa2dec26b60
Vadossss <iv.gav789@mail.ru>
2025-03-27 15:12:35 +0300

    Добавлен класс

---
+++ backend/src/main/java/com/search/backend/security/MyUserDetails.java
@@ -0,0 +1,41 @@
+package com.search.backend.security;
+
+import com.search.backend.models.AppUser;
+import com.search.backend.repositories.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetails implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public MyUserDetails(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        final AppUser appUser = userRepository.findByUsername(username);
+
+        if (appUser == null) {
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        }
+
+        System.out.println(appUser.getRole());
+
+        return User
+                .withUsername(username)
+                .password(appUser.getPassword())
+                .authorities(appUser.getRole())
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+
+}
