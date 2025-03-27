64a87483014c1cc30d84e09885126bc9ac039d89
Vadossss <iv.gav789@mail.ru>
2025-03-27 12:56:22 +0300

    Добавлена модель данных для добавления нового элемента в категории пользователя

---
+++ backend/src/main/java/com/search/backend/models/NewItemToListRequest.java
@@ -0,0 +1,26 @@
+package com.search.backend.models;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Класс для работы с коллекциями пользователя
+ * Хранит информацию об идентификаторе пользователя, названии коллекции и добавляемому элементу в коллекцию
+ */
+@Getter
+@Setter
+public class NewItemToListRequest {
+    @NotBlank
+    @NotNull
+    private String userId;
+
+    @NotBlank
+    @NotNull
+    private String itemId;
+
+    @NotBlank
+    @NotNull
+    private String categoryName;
+}
