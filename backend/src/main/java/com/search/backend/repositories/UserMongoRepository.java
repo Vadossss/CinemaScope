94831eada723b872ec1975a099d1beb8193c4462
Vadossss <iv.gav789@mail.ru>
2025-03-27 12:53:51 +0300

    Добавлен репозиторий пользователя в MongoDB

---
+++ backend/src/main/java/com/search/backend/repositories/UserMongoRepository.java
@@ -0,0 +1,14 @@
+package com.search.backend.repositories;
+
+import com.search.backend.models.UserMongo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+/**
+ * Репозиторий для работы с пользователями в MongoDB
+ */
+
+public interface UserMongoRepository extends MongoRepository<UserMongo, Integer> {
+    Optional<UserMongo> findById(String userId);
+}
