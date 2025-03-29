21924c44b048e65206768f7cb9df3e71f32f34d4
Vadossss <iv.gav789@mail.ru>
2025-03-29 05:33:57 +0300

    Обновлён контроллер, добавлены новые эндпоинты

--- backend/src/main/java/com/search/backend/controllers/AuthController.java
+++ backend/src/main/java/com/search/backend/controllers/AuthController.java
@@ -3,8 +3,13 @@ package com.search.backend.controllers;
 import com.search.backend.models.AppUser;
 import com.search.backend.models.LoginRequest;
 import com.search.backend.models.RefreshTokenRequest;
+import com.search.backend.models.SetRoleRequest;
 import com.search.backend.services.AuthService;
+import com.search.backend.services.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,9 +17,11 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final UserService userService;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, UserService userService) {
         this.authService = authService;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
@@ -30,6 +37,22 @@ public class AuthController {
     @PostMapping("/updateRefreshToken")
     public ResponseEntity<Object> updateRefreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
         return authService.updateRefreshToken(refreshTokenRequest);
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @PostMapping("/setRole")
+    public ResponseEntity<Object> setUserRole(@RequestBody SetRoleRequest setRoleRequest) {
+        return authService.setUserRole(setRoleRequest.getUserId(), setRoleRequest.getRole());
+    }
+
+    @GetMapping("/debugRoles")
+    public ResponseEntity<?> debugRoles(Authentication authentication) {
+        return ResponseEntity.ok(authentication.getAuthorities());
+    }
+
+    @GetMapping("/current-user")
+    public UserDetails getCurrentUser() {
+        return userService.getCurrentUser();
     }
 }
 
