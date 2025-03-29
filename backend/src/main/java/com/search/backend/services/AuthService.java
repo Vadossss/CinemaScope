cce2ad1044f4ff294e65194d1f30cf64191113f2
Vadossss <iv.gav789@mail.ru>
2025-03-29 05:35:54 +0300

    Обновлён сервис, добавлена запись в монго и добавлена возможность смены роли пользователя

--- backend/src/main/java/com/search/backend/services/AuthService.java
+++ backend/src/main/java/com/search/backend/services/AuthService.java
@@ -3,11 +3,18 @@ package com.search.backend.services;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.search.backend.models.*;
+import com.search.backend.repositories.UserMongoRepository;
 import com.search.backend.repositories.UserRepository;
 import com.search.backend.security.JwtUtil;
+import jakarta.transaction.Transactional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @Service
@@ -16,12 +23,14 @@ public class AuthService {
     private final JwtUtil jwtUtil;
 
     private final UserRepository userRepository;
+    private final UserMongoRepository userMongoRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil, UserMongoRepository userMongoRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtUtil = jwtUtil;
+        this.userMongoRepository = userMongoRepository;
     }
 
     private AuthResponse generateAuthResponse(AppUser user) {
@@ -30,6 +39,7 @@ public class AuthService {
         return new AuthResponse(accessToken, refreshToken);
     }
 
+    @Transactional
     public ResponseEntity<Object> registerUser(AppUser user) {
         if (userRepository.existsByUsername(user.getUsername())) {
             return ResponseEntity.status(401).body("Username already exists");
@@ -43,7 +53,20 @@ public class AuthService {
 
         AuthResponse authResponse = generateAuthResponse(user);
 
-        userRepository.save(user);
+        AppUser savedUser = userRepository.save(user);
+
+        UserMongo userMongo = new UserMongo();
+        userMongo.setUsername(user.getUsername());
+        userMongo.setId(String.valueOf(savedUser.getId()));
+
+        Map<String, List<String>> categories = new HashMap<>();
+        categories.put("watched", new ArrayList<>());
+        categories.put("planned", new ArrayList<>());
+        categories.put("dropped", new ArrayList<>());
+        categories.put("watching", new ArrayList<>());
+        userMongo.setCategories(categories);
+
+        userMongoRepository.save(userMongo);
 
         return ResponseEntity.ok(authResponse);
     }
@@ -63,13 +86,12 @@ public class AuthService {
 
     public ResponseEntity<Object> updateRefreshToken(RefreshTokenRequest refreshTokenRequest) {
         try {
-            String refreshToken = refreshTokenRequest.getRefreshToken();
 
             DecodedJWT decodedJWT = jwtUtil.validateToken(refreshTokenRequest.getRefreshToken());
 
             String username = decodedJWT.getSubject();
             String accessToken = jwtUtil.generateAccessToken(username);
-
+            String refreshToken = jwtUtil.generateRefreshToken(username);
             return ResponseEntity.ok(new AuthResponse(accessToken, refreshToken));
         }
         catch (JWTVerificationException e) {
@@ -79,6 +101,16 @@ public class AuthService {
 
     private boolean passwordMatches(String password, String hashedPassword) {
         return passwordEncoder.matches(password, hashedPassword);
+    }
+
+    public ResponseEntity<Object> setUserRole(Integer userId, String nameRole) {
+        AppUser user = userRepository.findById(userId).orElse(null);
+        if (user == null) {
+            return ResponseEntity.status(401).body("User not found");
+        }
+        user.setRole(AppUserRole.valueOf(nameRole));
+        userRepository.save(user);
+        return ResponseEntity.ok().body("Successfully updated role");
     }
 
 }
