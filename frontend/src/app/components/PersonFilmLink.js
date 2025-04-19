21ab73b219675ee36933fc929a806b7414d9a367
Vadossss <iv.gav789@mail.ru>
2025-04-19 09:10:45 +0300

    Обновлён компонент, добавлен скелет загрузки изображения

--- frontend/src/app/components/PersonFilmLink.js
+++ frontend/src/app/components/PersonFilmLink.js
@@ -6,16 +6,16 @@ import {
   Popover,
   PopoverHandler,
   PopoverContent,
-  Button,
-  Avatar,
-  Typography,
 } from "@material-tailwind/react";
+
+import {Skeleton} from "@heroui/react";
 
 import Link from "next/link";
 import { useState } from "react";
 
 export default function App({ data }) {
   const [openPopover, setOpenPopover] = useState(false);
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   const triggers = {
     onMouseEnter: () => setOpenPopover(true),
@@ -35,15 +35,24 @@ export default function App({ data }) {
       </PopoverHandler>
       <PopoverContent {...triggers} className="z-50 max-w-[24rem]">
         <div className="mb-2 flex justify-between gap-4">
-          <div>
-            <img className="h-[180px] rounded" src={data.photo}></img>
+          <div className="relative h-[180px] w-[120px]">
+            <img
+                src={data.photo}
+                alt={data.name ?? data.enName}
+                onLoad={() => setImageLoaded(true)} // здесь ловим событие
+                className={`h-full w-full rounded-lg object-cover transition-opacity duration-700 ${imageLoaded ? 'opacity-100' : 'opacity-0'}`}
+            />
+            {!imageLoaded && (
+                <Skeleton className="absolute inset-0 h-full w-full rounded-lg"/>
+            )}
           </div>
           <div className="flex flex-col items-top">
             <div>
               <p className="font-bold text-lg">{data.name !== null ? data.name : data.enName}</p>
             </div>
             <div>
-              <span className="font-light text-gray-500">{data.profession === "актеры" ? "Актёр" : data.profession === "актеры дубляжа" ? "Актёр дубляжа" : "Неизвестно"}</span>
+              <span
+                  className="font-light text-gray-500">{data.profession === "актеры" ? "Актёр" : data.profession === "актеры дубляжа" ? "Актёр дубляжа" : "Неизвестно"}</span>
             </div>
           </div>
         </div>
