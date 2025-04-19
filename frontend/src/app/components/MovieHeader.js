d21730d3c0925febea7752168ea9dd8900324f51
Vadossss <iv.gav789@mail.ru>
2025-04-19 09:09:15 +0300

    Обновлён компонент

--- frontend/src/app/components/MovieHeader.js
+++ frontend/src/app/components/MovieHeader.js
@@ -1,6 +1,6 @@
-import {Accordion, AccordionItem, Button} from "@heroui/react";
-import Link from "next/link";
+import {Button} from "@heroui/react";
 import RateMovieButton from "@/app/components/RateMovieButton";
+import WhereToWatchAccordion from "./WhereToWatchAccordion"
 
 export const Bookmark = () => {
     return (
@@ -29,21 +29,7 @@ export default function MovieHeader({filmData}) {
                     </Button>
                 </div>
                 <div className="w-[160px]">
-                    <Accordion className="p-0">
-                        <AccordionItem className="flex flex-col p-0" aria-label="Где посмотреть?" title="Где смотреть?">
-                            <div className="flex flex-col gap-3">
-                                {filmData.watchability?.items.map((data, index) => (
-                                    <div key={index}>
-                                        <Link className="flex items-center gap-2" href={data.url}>
-                                            <img className="w-8 h-8 rounded-full" src={data.logo.url}></img>
-                                            <span
-                                                className="hover:text-orange-600 transition duration-200 ease-in-out">{data.name}</span>
-                                        </Link>
-                                    </div>
-                                ))}
-                            </div>
-                        </AccordionItem>
-                    </Accordion>
+                    <WhereToWatchAccordion filmData={filmData} />
                 </div>
             </div>
             <div className="flex flex-col">
