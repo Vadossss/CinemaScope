4b9a244062f692d00a3317064a812263ba8b6841
Vadossss <iv.gav789@mail.ru>
2025-04-19 08:03:00 +0300

    Создан сервис для реализации логики связанной с комментариями

---
+++ backend/src/main/java/com/search/backend/services/CommentService.java
@@ -0,0 +1,29 @@
+package com.search.backend.services;
+
+import com.search.backend.models.CommentMongo;
+import com.search.backend.repositories.CommentRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final MongoTemplate mongoTemplate;
+
+    public CommentService(CommentRepository commentRepository, MongoTemplate mongoTemplate) {
+        this.commentRepository = commentRepository;
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    public ResponseEntity<Object> getComments(int movieId) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("movieId").is(movieId));
+        query.with(Sort.by(Sort.Direction.DESC, "commentId"));
+        return ResponseEntity.ok(mongoTemplate.find(query, CommentMongo.class));
+    }
+}
