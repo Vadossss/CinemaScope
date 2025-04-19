ac14ca6fafcbcfa5ccad7c61a34eb6d3c7286924
Vadossss <iv.gav789@mail.ru>
2025-04-19 07:12:45 +0300

    Обновлен компонент карты фильма

--- frontend/src/app/components/MovieCard.js
+++ frontend/src/app/components/MovieCard.js
@@ -1,24 +1,39 @@
 import Link from "next/link";
+import { fetchAddWishlist } from "../utils/fetchAddWishlist";
+import { addToast, Button } from "@heroui/react";
 
 export default function App({ data }) {
+
 
     const ratingColor = data.rating.kp > 7.1 ? "#3bb33b" : data.rating.kp > 4.1 ? "#777" : "#ff1414";
 
     return (
-        <div className="w-[150px]">
-            <Link href="/">
-                {data?.poster?.previewUrl !== null && data?.poster?.previewUrl ? (
-                    <img className="object-contain h-[225px]" src={data.poster.previewUrl} alt="Постер" />
-                ) : (
-                    <img className="object-contain h-[225px]" src="icon_movie.svg" alt="Постер" />
-                )}
-
-                <div className={`absolute absolute top-2 start-2 pr-2 pl-2 text-sm`}
-                    style={{ backgroundColor: ratingColor }}>
-                    <p>{(data.rating.kp).toFixed(1)}</p>
+        <div className="w-[150px] group/item">
+            <Link href={data.isSeries ? `/series/${data.id}` : `/films/${data.id}`}>
+                <div className="relative">
+                    <div className="absolute inset-0 bg-black bg-opacity-20 opacity-0 group-hover/item:opacity-100 transition-opacity rounded"></div>
+                    {data?.poster?.previewUrl !== null && data?.poster?.previewUrl ? (
+                        <img className="object-fit h-[225px] rounded" src={data.poster.previewUrl} alt="Постер" />
+                    ) : (
+                        <img className="object-contain h-[225px] rounded" src="icon_movie.svg" alt="Постер" />
+                    )}
                 </div>
+
+                <div className={`absolute absolute top-2 start-2 pr-2 pl-2 text-sm rounded-sm`}
+                    style={{ backgroundColor: ratingColor }}>
+                    <p className="text-white font-bold">{(data.rating.kp).toFixed(1)}</p>
+                </div>
+
+                <div className={`absolute top-[7px] right-4 pr-2 pl-2 text-sm rounded-sm invisible group-hover/item:visible`}>
+                    <button
+                        onClick={() => fetchAddWishlist(data.id, "planned")}
+                        title="Буду смотреть">
+                        <img className="-scale-100" src="icon_add.svg"></img>
+                    </button>
+                </div>
+
                 <div className="flex flex-col mt-1">
-                    <span className="text-base font-medium">{data.name !== null ? data.name : data.alternativeName}</span>
+                    <span className="text-base font-medium line-clamp-2 group-hover/item:text-orange-700">{data.name !== null ? data.name : data.alternativeName}</span>
                     <span className="text-xs font-thin text-gray-500">{data.year}{Array.isArray(data.genres) && data.genres[0]?.name ? `, ` + data.genres[0].name : ""}</span>
                 </div>
             </Link>
