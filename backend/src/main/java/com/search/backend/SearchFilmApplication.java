6e9ba833db276455cbf8980500c783b4c4e486e3
Vadossss <iv.gav789@mail.ru>
2025-04-19 08:08:56 +0300

    Обновлён исполняемый файл

--- backend/src/main/java/com/search/backend/SearchFilmApplication.java
+++ backend/src/main/java/com/search/backend/SearchFilmApplication.java
@@ -2,20 +2,10 @@ package com.search.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
-@EnableKafka
 @SpringBootApplication
 public class SearchFilmApplication {
-
-//	@KafkaListener(topics="msg")
-//	public void msgListener(String msg){
-//		System.out.println(msg);
-//	}
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(SearchFilmApplication.class, args);
 	}
-
 }
