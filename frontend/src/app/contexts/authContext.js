c9c505edf39558a55ac8daf9ea82a58e6be19896
Vadossss <iv.gav789@mail.ru>
2025-04-19 07:10:55 +0300

    Обновлён контекст

--- frontend/src/app/contexts/authContext.js
+++ frontend/src/app/contexts/authContext.js
@@ -2,25 +2,54 @@ import { createContext, useState, useEffect, useContext } from "react";
 
 const AuthContext = createContext();
 
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL
+
 export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState(false);
+    const [auth, setAuth] = useState(null);
+    const [role, setRole] = useState(null);
     const [username, setUsername] = useState(null)
+    const [userId, setUserId] = useState(null)
 
     useEffect(() => {
         const checkMe = async () => {
             try {
-                const res = await fetch("http://localhost:8085/auth/me", {
+                const res = await fetch(`${BASE_URL}/auth/me`, {
                     method: "GET",
                     credentials: "include"
                 });
 
                 if (res.ok) {
-                    const data = await res.text();
-                    setUsername(data);
+                    const data = await res.json();
+                    setUsername(data.username);
+                    setRole(data.role);
+                    setUserId(data.id);
                     setAuth(true);
                 }
-                else {
-                    setAuth(false);
+                else if (res.status === 403) {
+                    console.log("Я здесь");
+
+                    const updateRefreshToken = async () => {
+                        try {
+                            const res = await fetch(`${BASE_URL}/auth/updateRefreshToken`, {
+                                method: "POST",
+                                credentials: "include"
+                            });
+
+                            if (res.ok) {
+                                checkMe();
+                            }
+                            else if (res.status === 401) {
+
+
+                                setAuth(false);
+                            }
+                        } catch (error) {
+                            console.log(error);
+                        }
+                    }
+                    updateRefreshToken();
+
+                    // setAuth(false);
                 }
             } catch (error) {
                 console.log(error);
@@ -30,7 +59,7 @@ export const AuthProvider = ({ children }) => {
     }, []);
 
     return (
-        <AuthContext.Provider value={{ auth, setAuth, username }}>
+        <AuthContext.Provider value={{ auth, setAuth, username, role, userId }}>
             {children}
         </AuthContext.Provider>
     )
