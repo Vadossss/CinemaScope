16baf68235c074f2a616aec9725483107f10d4b5
Vadossss <iv.gav789@mail.ru>
2025-04-19 07:55:29 +0300

    Обновлена response модель для работы с access токеном

--- backend/src/main/java/com/search/backend/models/AuthResponse.java
+++ backend/src/main/java/com/search/backend/models/AuthResponse.java
@@ -1,13 +1,13 @@
 package com.search.backend.models;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class AuthResponse {
     private String accessToken;
-    private String refreshToken;
+    public AuthResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
 }
