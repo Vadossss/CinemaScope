e88f04c8ea4452fae81cb847ac8385e6d8036c39
Vadossss <iv.gav789@mail.ru>
2025-05-26 03:38:34 +0300

    Логика получения данных вынесена в родительский компонент

--- frontend/src/app/components/PopularMovieSwiper.js
+++ frontend/src/app/components/PopularMovieSwiper.js
@@ -1,32 +1,9 @@
-import { useEffect, useState } from "react";
 import ScrollMovie from "./ScrollMovie";
-import { fetchPopularMovie } from '@/app/utils/fetchPopularMovie';
-import { Spinner } from "@heroui/react";
 
-export default function App() {
-    const [movieData, setMovieData] = useState(null);
-    const [isLoading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const [result, isLoading] = await fetchPopularMovie();
-            setMovieData(result);
-            setLoading(isLoading);
-        };
-
-        fetchData();
-    }, []);
-
-    if (isLoading) {
-        return (
-            <div className="flex justify-center items-center">
-                <Spinner color="default" />
-            </div>
-        )
-    }
+export default function App({movieData}) {
 
     return (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 w-[1504px]">
             <span className="font-bold">Популярные новинки</span>
             <ScrollMovie data={movieData} />
         </div>
