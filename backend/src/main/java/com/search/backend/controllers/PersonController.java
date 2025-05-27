65c98b6351a603e40da737b166bc2c7220017f00
GabberSlam <egor20033084@gmail.com>
2025-05-28 02:17:27 +0300

    Добавлен эндпоинт для вывода всех актёров

--- backend/src/main/java/com/search/backend/controllers/PersonController.java
+++ backend/src/main/java/com/search/backend/controllers/PersonController.java
@@ -6,9 +6,13 @@ import com.search.backend.models.MovieMongo;
 import com.search.backend.models.PersonMongo;
 import com.search.backend.repositories.PersonRepositoryMongo;
 import com.search.backend.services.PersonService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
 
 import java.io.IOException;
 
@@ -28,7 +32,7 @@ public class PersonController {
 
     @GetMapping("/save")
     public void saveExampleMovie() throws IOException {
-        for (int page = 5; page <= 250; page++) {
+        for (int page = 6; page <= 15; page++) {
             String jo = personService.getPersons(page);
             JsonNode js = (new ObjectMapper()).readTree(jo);
             for (JsonNode j : js.path("docs")) {
@@ -42,4 +46,15 @@ public class PersonController {
             }
         }
     }
+
+    @GetMapping("/getPersons")
+    public Page<PersonMongo> getPersons(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "12") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        return personRepository.findAll(pageable);
+    }
+
+
 }
