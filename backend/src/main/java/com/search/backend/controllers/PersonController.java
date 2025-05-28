28fa233b0e76cf27e71645a88b4b95d6fa25f408
GabberSlam <egor20033084@gmail.com>
2025-05-28 04:37:14 +0300

    Эндпоинт для отображения обновлён

--- backend/src/main/java/com/search/backend/controllers/PersonController.java
+++ backend/src/main/java/com/search/backend/controllers/PersonController.java
@@ -8,10 +8,7 @@ import com.search.backend.repositories.PersonRepositoryMongo;
 import com.search.backend.services.PersonService;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Page;
 
 import java.io.IOException;
