2fb9b10f7f330b625ab8259e162fad59b7a775c8
GabberSlam <egor20033084@gmail.com>
2025-05-29 14:50:51 +0300

    Добавлен fetch для получения персон по ID

---
+++ frontend/src/app/utils/fetchGetPersonByID.js
@@ -0,0 +1,26 @@
+export async function getMovieById(id) {
+    try{
+        const res = await fetch(`http://localhost:8085/person/findPersonByID?id=${id}`,{
+            method: "GET",
+            credentials: "include",
+            headers:{
+                "Content-Type": "application/json"
+            },
+        });
+
+
+        if (res.ok) {
+            return [await res.json(), false];
+        }
+
+        else if (res.status === 404) {
+            console.log("Ошибка");
+            return [null, false]
+        }
+        
+        } catch (error) {
+            console.log("Error");
+            return [, null];
+        }
+}
+    
