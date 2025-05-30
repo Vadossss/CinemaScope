5ca97a5dc5cf99cd3565f1a81ca8ff9af937a86b
GabberSlam <egor20033084@gmail.com>
2025-05-30 05:05:14 +0300

    Создан и изменёён компонент карточки персоны

---
+++ frontend/src/app/components/PersonPage.js
@@ -0,0 +1,68 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import PersonHeader from "./PersonHeader";
+import PersonBody from "./PersonBody";
+import PersonMovieList from "./PersonMovieList";
+import { fetchGetPersonByID } from "../utils/fetchGetPersonByID";
+
+export default function PersonPage() {
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+
+  const [person, setPerson] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!id) {
+      setError("ID не указан");
+      setLoading(false);
+      return;
+    }
+
+    async function fetchPerson() {
+      const [data, hasError] = await fetchGetPersonByID(id);
+
+      if (hasError) {
+        setError("Ошибка загрузки данных.");
+        setPerson(null);
+      } else {
+        setPerson(data);
+        setError(null);
+      }
+
+      setLoading(false);
+    }
+
+    fetchPerson();
+  }, [id]);
+
+  if (loading) return <div>Загрузка...</div>;
+  if (error) return <div style={{ color: "red" }}>Ошибка: {error}</div>;
+  if (!person) return <div>Персона не найдена</div>;
+
+  return (
+    <div className="min-h-screen w-full p-6 bg-gray-50">
+      <div className="w-full bg-white rounded shadow p-6 flex flex-col gap-6">
+        {/* Секция информации */}
+        <div className="flex flex-col md:flex-row gap-6">
+          {/* Левая часть (фото и имя) */}
+          <div className="w-full md:w-1/3">
+            <PersonHeader person={person} />
+          </div>
+
+          {/* Правая часть (данные) */}
+          <div className="w-full md:w-2/3">
+            <PersonBody person={person} />
+          </div>
+        </div>
+
+        {/* Фильмы */}
+        <PersonMovieList movies={person.movies} />
+      </div>
+    </div>
+  );
+}
