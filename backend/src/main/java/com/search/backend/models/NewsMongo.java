38c53fdbf593b5700f02de724ea7474643bab09c
GabberSlam <egor20033084@gmail.com>
2025-06-01 03:24:16 +0300

    Создана и изменена модель для новостей

---
+++ backend/src/main/java/com/search/backend/models/NewsMongo.java
@@ -0,0 +1,39 @@
+package com.search.backend.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.Date;
+import java.util.*;
+
+
+@Getter
+@Setter
+@Document(collection = "news")
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class NewsMongo {
+
+    @Id
+    private Long kinopoiskId;
+
+    @Field(write = Field.Write.ALWAYS)
+    private String imageUrl;
+
+    @Field(write= Field.Write.ALWAYS)
+    private String title;
+
+    @Field(write = Field.Write.ALWAYS)
+    private String description;
+
+    @Field(write = Field.Write.ALWAYS)
+    private String url;
+
+    @Field(write = Field.Write.ALWAYS)
+    private String publishedAt;
+}
