a34df4ff384220cddce91b7b29bce6583c338ad2
Vadossss <iv.gav789@mail.ru>
2025-06-01 21:09:50 +0300

    Добавлен класс для ответа в виде модели

---
+++ backend/src/main/java/com/search/backend/models/MessageResponse.java
@@ -0,0 +1,12 @@
+package com.search.backend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
