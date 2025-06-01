94b79b0e00f2e69d5fa8fc466e8f451f40e8c891
Vadossss <iv.gav789@mail.ru>
2025-06-01 21:21:50 +0300

    Обновлен security config

--- backend/src/main/java/com/search/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/search/backend/security/SecurityConfig.java
@@ -41,7 +41,8 @@ public class SecurityConfig {
                         .requestMatchers("/auth/register", "/auth/login", "/auth/updateRefreshToken",
                                 "/auth/debugRoles", "/films/movie", "/films/getMovieById",
                                 "/comments/getCommentsForMovie", "films/popularNow", "/films/findByRegex",
-                                "/person/getPersons", "/person/findPersonByID","news/save", "/news/getNews").permitAll()
+                                "/person/getPersons", "/person/findPersonByID","news/save", "/news/getNews",
+                                "/user/getUserProfile").permitAll()
                         .anyRequest().authenticated()
                 )
 
