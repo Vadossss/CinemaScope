eba7f0030db7a6067727b5efe4c084eac40ce3bc
GabberSlam <egor20033084@gmail.com>
2025-06-01 06:49:35 +0300

    Изменён репозиторий для новостей

--- backend/src/main/java/com/search/backend/repositories/NewsRepositoryMongo.java
+++ backend/src/main/java/com/search/backend/repositories/NewsRepositoryMongo.java
@@ -1,8 +1,14 @@
 package com.search.backend.repositories;
 
 import com.search.backend.models.NewsMongo;
+import org.hibernate.query.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Date;
 
 public interface NewsRepositoryMongo extends MongoRepository<NewsMongo, Long> {
+
 
 }
