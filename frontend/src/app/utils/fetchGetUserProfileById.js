90c6b1d4b2227d15443f8d7943938fb183e26256
Vadossss <iv.gav789@mail.ru>
2025-06-01 08:03:38 +0300

    Добавлена fetch функция для получения профиля пользователя по id

---
+++ frontend/src/app/utils/fetchGetUserProfileById.js
@@ -0,0 +1,22 @@
+export async function fetchGetUserProfileById(id) {
+    try {
+        const res = await fetch(`http://localhost:8085/user/getUserProfile?userId=${id}`, {
+            method: "GET",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        });
+
+        if (res.ok) {
+            return [await res.json(), false];
+        }
+        else if (res.status === 404) {
+            console.log("Ошибка");
+            return [await res.text(), false]
+        }
+    } catch (error) {
+        console.log("Error");
+        return [, null];
+    }
+}
