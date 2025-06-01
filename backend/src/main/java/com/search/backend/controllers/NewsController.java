51fd50805d5617d721224734e3cd44800d46864e
GabberSlam <egor20033084@gmail.com>
2025-06-01 06:42:04 +0300

    Изменёнено количество спаршенных страниц

--- backend/src/main/java/com/search/backend/controllers/NewsController.java
+++ backend/src/main/java/com/search/backend/controllers/NewsController.java
@@ -3,10 +3,18 @@ package com.search.backend.controllers;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.search.backend.models.NewsMongo;
+import com.search.backend.models.PersonMongo;
 import com.search.backend.repositories.NewsRepositoryMongo;
 import com.search.backend.services.NewsService;
+import lombok.Builder;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -25,7 +33,7 @@ public class NewsController {
 
     @GetMapping("/save")
     public void saveNewsExample() throws IOException {
-        for (int page = 10; page <= 50; page++) {
+        for (int page = 1; page <= 98; page++) {
             String jo = newsService.getNews(page);
             JsonNode js = (new ObjectMapper()).readTree(jo);
             for (JsonNode j : js.path("items")) {
@@ -39,4 +47,15 @@ public class NewsController {
             }
         }
     }
+    @GetMapping("/getNews")
+    public Page<NewsMongo> getNews(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "12") int size,
+            @RequestParam(defaultValue = "desc") String sortDirection // по умолчанию по убыванию
+    ) {
+        Sort.Direction direction = Sort.Direction.fromOptionalString(sortDirection.toUpperCase()).orElse(Sort.Direction.DESC);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, "publishedAt"));
+        return newsRepository.findAll(pageable);
+    }
+
 }
