1eedab9121cd8bc53e927981ee6525c7d44130ea
GabberSlam <egor20033084@gmail.com>
2025-06-02 04:41:41 +0300

    Изменён контроллер Админа: добавлены эндпоинты для поиска пользователя по имени и выдача ему ролей

--- backend/src/main/java/com/search/backend/controllers/AdminController.java
+++ backend/src/main/java/com/search/backend/controllers/AdminController.java
@@ -1,21 +1,29 @@
 package com.search.backend.controllers;
 
+import com.search.backend.models.AppUser;
+import com.search.backend.models.ForAdminInfoModel;
+import com.search.backend.models.SetRoleRequest;
+import com.search.backend.models.UserMongo;
+import com.search.backend.repositories.UserRepository;
 import com.search.backend.services.AdminService;
+import org.apache.kafka.clients.admin.Admin;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
 
     private final AdminService adminService;
+    private UserRepository userRepository;
 
-    public AdminController(AdminService adminService) {
+    public AdminController(AdminService adminService, UserRepository userRepository) {
         this.adminService = adminService;
+        this.userRepository = userRepository;
     }
 
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
@@ -23,4 +31,19 @@ public class AdminController {
     public ResponseEntity<String> deleteComment(@RequestParam String id) {
         return adminService.deleteComment(id);
     }
+
+    @GetMapping("/findByName")
+    public List<ForAdminInfoModel> findByName(@RequestParam String name, @RequestParam(defaultValue = "10") int limit) {
+        List<AppUser> users = userRepository.findByUsernameContainingIgnoreCase(name);
+        return users.stream().map(user->{
+            return new ForAdminInfoModel(user.getId(), user.getUsername(), user.getRole());
+        }).collect(Collectors.toList());
+    }
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @PostMapping("/setRole")
+    public ResponseEntity<Object> setUserRole(@RequestBody SetRoleRequest setRoleRequest) {
+        return adminService.setUserRole(setRoleRequest.getUserId(), setRoleRequest.getRole());
+    }
+
+
 }
