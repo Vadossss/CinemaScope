6c2abf0094fb0ba0416ebff6d4b87c97d2ebc637
Vadossss <iv.gav789@mail.ru>
2025-06-03 08:20:29 +0300

    Обновлена страница, добавлено отображение аватара пользователя

--- frontend/src/app/user/[userId]/page.js
+++ frontend/src/app/user/[userId]/page.js
@@ -1,4 +1,5 @@
 "use client"
+
 import React, { useState, useEffect } from "react";
 import { fetchGetUserProfileById } from "@/app/utils/fetchGetUserProfileById";
 import {useParams, useRouter} from "next/navigation";
@@ -11,19 +12,25 @@ import GenresGraphScore from "../../components/GenresGraphScore";
 import GraphMovieByYear from "../../components/GraphMoviesByYear";
 import MovieListForUser from "../../components/MovieListForUser";
 import {CategoriesContextProvider} from "@/app/contexts/categoriesMoviesContext";
+import {useAuth} from "@/app/contexts/authContext";
+import SettingsModal from "../../components/SettingsModal";
 
 const App = () => {
     const [userData, setUserData] = useState(null);
     const [isLoading, setLoading] = useState(true);
     const [isUpdate, setUpdate] = useState(false);
     const params = useParams();
-    const userId = params.userId;
+    const paramsUserId = params.userId;
     const router = useRouter();
+    const { avatar, auth, userId } = useAuth();
+    const [avatarUrl, setAvatarUrl] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
-            const [result, isLoading] = await fetchGetUserProfileById(userId);
+            const [result, isLoading] = await fetchGetUserProfileById(paramsUserId);
             setUserData(result);
+            setAvatarUrl(result?.avatarUrl)
+            console.log(result);
             setLoading(false);
             setUpdate(false);
         }
@@ -44,6 +51,14 @@ const App = () => {
         )
     }
 
+    const getAvatarSrc = () => {
+        if (paramsUserId === userId) {
+            return avatar || "/icon_avatar1.png";
+        } else {
+            return avatarUrl || "/icon_avatar1.png";
+        }
+    };
+
     return (
         <div className="w-[1600px] min-h-screen bg-white rounded-xl text-white">
             <main className="container mx-auto px-4 py-10">
@@ -51,11 +66,14 @@ const App = () => {
                     className="bg-blue-950 bg-opacity-60 rounded-xl p-6 mb-5 backdrop-blur-sm shadow-lg shadow-orange-500">
                     <div className="flex flex-col md:flex-row items-center md:items-start gap-6">
                         <div className="w-32 h-32 rounded-full overflow-hidden border-4 border-orange-500 shadow-lg">
-                            <img src="/icon_avatar1.png" alt="User Avatar"
+                            <img src={getAvatarSrc()} alt="User Avatar"
                                  className="w-full h-full object-cover"/>
                         </div>
                         <div className="flex flex-col space-y-4">
                             <h2 className="text-2xl md:text-3xl font-bold">{userData.user}</h2>
+                            {userId === paramsUserId && (
+                                <SettingsModal />
+                            )}
                             <div className="flex gap-1">
                                 {/*<p className="text-white opacity-95 max-w-lg">Любимые жанры: </p>*/}
                                 {/*<span className="font-bold text-orange-500">{user.favoriteGenres.join(", ")}</span>*/}
