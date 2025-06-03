fab032583b7ddfc71082ce91f96057f4f64ce19f
Vadossss <iv.gav789@mail.ru>
2025-06-03 08:08:54 +0300

    Добавлена аннотация для подключения конфигурационных свойств класса

--- backend/src/main/java/com/search/backend/SearchFilmApplication.java
+++ backend/src/main/java/com/search/backend/SearchFilmApplication.java
@@ -1,9 +1,12 @@
 package com.search.backend;
 
+import com.search.backend.models.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(FileStorageProperties.class)
 public class SearchFilmApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SearchFilmApplication.class, args);
