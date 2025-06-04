2d2a457c769e3c7ec6d8b72d382492c4200ecb83
Vadossss <iv.gav789@mail.ru>
2025-06-04 11:13:49 +0300

    Обновлена страница фильма, убран лишний div

--- frontend/src/app/films/[filmId]/page.js
+++ frontend/src/app/films/[filmId]/page.js
@@ -2,8 +2,6 @@ import FilmPage from "@/app/components/FilmPage";
 
 export default function App() {
     return (
-        <div>
-            <FilmPage />
-        </div>
+        <FilmPage />
     )
 }
