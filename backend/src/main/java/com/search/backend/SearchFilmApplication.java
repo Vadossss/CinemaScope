3dfeea009309f471f62aa6a8b828dfc92eac2474
Vadossss <iv.gav789@mail.ru>
2025-06-04 10:56:55 +0300

    Обновлен исполняемый класс

--- backend/src/main/java/com/search/backend/SearchFilmApplication.java
+++ backend/src/main/java/com/search/backend/SearchFilmApplication.java
@@ -3,9 +3,20 @@ package com.search.backend;
 import com.search.backend.models.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories(
+		basePackages = "com.search.backend.repositories.jpa"
+)
+@EnableMongoRepositories(
+		basePackages = "com.search.backend.repositories.mongo"
+)
+@EntityScan(basePackages = "com.search.backend.models")
 @EnableConfigurationProperties(FileStorageProperties.class)
 public class SearchFilmApplication {
 	public static void main(String[] args) {
