30a721796bed6e7b74a372ad942ce9a9d8295856
Vadossss <iv.gav789@mail.ru>
2025-06-04 11:18:18 +0300

    Обновлен компонент шапки фильма, добавлена адаптивность

--- frontend/src/app/components/MovieHeader.js
+++ frontend/src/app/components/MovieHeader.js
@@ -1,6 +1,9 @@
-import {Button} from "@heroui/react";
+"use client"
+
 import RateMovieButton from "@/app/components/RateMovieButton";
 import WhereToWatchAccordion from "./WhereToWatchAccordion"
+import ButtonAddToLists from "./ButtonAddToLists";
+import {useEffect, useState} from "react";
 
 export const Bookmark = () => {
     return (
@@ -10,55 +13,75 @@ export const Bookmark = () => {
 
 export default function MovieHeader({filmData}) {
     const ratingColor = filmData.rating.kp > 7.1 ? "#3bb33b" : filmData.rating.kp > 4.1 ? "#777" : "#ff1414";
+    const [score, setScore] = useState(filmData.rating.rate || 0);
+    const [countScores, setCountScores] = useState(filmData.votes.vote || 0);
+    const [newCount, setNewCount] = useState(false);
+    const [usRatingColor, setUsRatingColor] = useState(filmData.rating.rate > 7.1 ? "#3bb33b" : filmData.rating.rate > 4.1 ? "#777" : "#ff1414");
+
+    useEffect(() => {
+        setUsRatingColor(score > 7.1 ? "#3bb33b" : score > 4.1 ? "#777" : "#ff1414");
+    }, [score]);
+
+    useEffect(() => {
+        if (newCount) {
+            setCountScores(countScores + 1);
+        }
+    }, [score])
+
+    // useEffect(() => {
+    //     setScore(filmData.rating.rate);
+    //     setCountScores(filmData.votes.vote);
+    // }, [filmData]);
+
     return (
-        <div className="flex gap-10">
-            <div className="flex flex-col gap-2 w-[600px]">
-                <div>
-                    <h1>
-                        <span
-                            className="text-4xl font-bold">{filmData.name !== null ? filmData.name : filmData.alternativeName} {filmData.year && `(${filmData.year})`}</span>
-                    </h1>
-                </div>
-                <div>
-                    <span
-                        className="text-gray-500">{filmData.ageRating !== null ? `${filmData.ageRating}+` : filmData.alternativeName}</span>
-                </div>
-                <div>
-                    <Button className="bg-gray-300 pr-3 pl-2" startContent={<Bookmark/>} variant="bordered">
-                        Буду смотреть
-                    </Button>
-                </div>
-                <div className="w-[160px]">
+        <div className="flex flex-col lg:flex-row gap-6 lg:gap-10">
+            <div className="flex flex-col gap-2 flex-1 max-w-full lg:max-w-[600px]">
+                <h1>
+          <span className="text-3xl sm:text-4xl font-bold">
+            {filmData.name ?? filmData.alternativeName} {filmData.year && `(${filmData.year})`}
+          </span>
+                </h1>
+                <span className="text-gray-500 text-sm">
+          {filmData.ageRating ? `${filmData.ageRating}+` : filmData.alternativeName}
+        </span>
+                {/*<Button className="bg-gray-300 pr-3 pl-2 w-max" startContent={<Bookmark />} variant="bordered">*/}
+                {/*    Буду смотреть*/}
+                {/*</Button>*/}
+                <ButtonAddToLists movieId={filmData.id}/>
+                <div className="w-full max-w-[160px] mt-2">
                     <WhereToWatchAccordion filmData={filmData} />
                 </div>
             </div>
-            <div className="flex flex-col">
-                <div>
-                    <h1>
+
+            <div className="flex flex-col items-start sm:items-center lg:items-start gap-2">
+                <div className="flex flex-col gap-2">
+                    <div className="flex flex-col">
                         {filmData.rating.kp > 0 ? (
-                            <span
-                                className="text-4xl font-bold"
-                                style={{color: ratingColor}}
-                            >{(filmData.rating.kp).toFixed(1)}
+                            <span className="text-4xl font-bold" style={{ color: ratingColor }}>
+                                {filmData.rating.kp.toFixed(1)}
                             </span>
                         ) : (
-                            <div className="flex flex-col">
-                                <span
-                                    className="text-4xl font-bold"
-                                >-</span>
-                                {/*<span*/}
-                                {/*    className="text-xs line-clamp-3 leading-5"*/}
-                                {/*>Недостаточно оценок, рейтинг формируется</span>*/}
+                            <div className="flex flex-col items-center">
+                                <span className="text-4xl font-bold">-</span>
                             </div>
                         )}
-                    </h1>
+                        <span className="text-xs font-thin text-gray-500">{filmData.votes.kp.toLocaleString("ru-RU")} оценок</span>
+                    </div>
+                    <div className="flex flex-col">
+                        {score > 0 ? (
+                            <span className="text-2xl font-bold" style={{ color: usRatingColor }}>
+                        {score.toFixed(1)}
+                  </span>
+                        ) : (
+                            <div className="flex flex-col items-center">
+                                <span className="text-4xl font-bold">-</span>
+                            </div>
+                        )}
+                        <span className="text-xs font-thin text-gray-500">{countScores.toLocaleString("ru-RU")} оценок</span>
+                    </div>
                 </div>
-                <div>
-                    <span
-                        className="text-xs font-thin text-gray-500">{filmData.votes.kp.toLocaleString("RU-ru")} оценок</span>
-                </div>
-                <RateMovieButton movieId={filmData.id}/>
+                <RateMovieButton setScore={setScore} setNewCount={setNewCount} movieId={filmData.id} />
             </div>
         </div>
-    )
+    );
 }
